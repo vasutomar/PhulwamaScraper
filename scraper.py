d1ca8d0f6b8ae4118acf9898e12776663256ab85
Vasu <vasutomar26@gmail.com>
2019-03-24 11:47:42 +0530

    graph rendered.

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import tweepy
 import json
 import wget
 import csv
+import networkx as nx
 
 #Twitter Authentication
 with open('twitter_credentials.json') as cred_data:
@@ -16,8 +17,9 @@ with open('twitter_credentials.json') as cred_data:
 	auth.set_access_token(access_token, access_secret)
 	api = tweepy.API(auth,wait_on_rate_limit=True)
 
-	number_of_tweets = 100
-	totalCount = 1
+	regular_tweet_count = 0
+	multimedia_tweets_count = 0
+	totalCount = 0
 
 	finalJson = "["
 	finalMultimediaJson = "["
@@ -25,7 +27,7 @@ with open('twitter_credentials.json') as cred_data:
 	firstIteration = True
 	lastIteration = False
 
-	hashtag_List = ["#phulwama","#phulwamaterrorattack","#SurgicalStrike2","#PhulwamaMartyrs","#crpfmartyrs","#PulwamaTerroristAttack"]
+	hashtag_List = ["#phulwama","#phulwamaterrorattack","#SurgicalStrike2","#PhulwamaMartyrs","#crpfmartyrs","#BalakotAirstrikes","#IndiaStrikesBack","#PulwamaTerroristAttack","#CRPF","#Balakot"]
 	testHashtag = ['#testingofscraper']
 
 	print("Exracting tweets")
@@ -35,22 +37,30 @@ with open('twitter_credentials.json') as cred_data:
 		hashtag = hashtag_List[i]
 		print("Processing "+hashtag)
 		multimedia_tweets_count = 1
+		totalCount = 0
 		for tweet in tweepy.Cursor(api.search,q=hashtag,lang="en").items():
-			if multimedia_tweets_count>=10000:
+			if multimedia_tweets_count>=10:
 				break
+
+			if totalCount>=50:
+				break
+
 			str = tweet._json
 			entity = str['entities']
 
 			json_string = json.dumps(str)
 
+			#print("total tweet count - ",totalCount)
+			totalCount+=1
+
 			if 'media' not in entity:
+				#print("regular tweet count = ",regular_tweet_count)
 				finalJson = finalJson+json_string+","
+				regular_tweet_count+=1
 			else:
+				print("multimedia tweet count",multimedia_tweets_count)
 				finalMultimediaJson = finalMultimediaJson+json_string+","
 				multimedia_tweets_count+=1
-
-	#print(finalJson)
-	#print(totalCount)
 	
 	finalJson = finalJson[:-1]
 	finalMultimediaJson = finalMultimediaJson[:-1]
@@ -68,3 +78,28 @@ with open('twitter_credentials.json') as cred_data:
 
 	with open('tweets.json','w') as outf:
 		json.dump(js,outf,indent = 4)
+
+	allTweetGraph = nx.Graph()
+	mulTweetGraph = nx.Graph()
+
+	tweetFile = open('tweets.json','r')
+	multiFile = open('multimedia.json','r')
+
+	tweets = json.load(tweetFile)
+	multim = json.load(multiFile)
+
+	#Adding all nodes in normal graph
+	for tweet in tweets:
+		userEntity = tweet['user']
+		username = userEntity['name']
+		allTweetGraph.add_node(username)
+
+	for mtweet in multim:
+		userEntity = mtweet['user']
+		username = userEntity['name']
+		mulTweetGraph.add_node(username)
+
+	nx.write_adjlist(allTweetGraph,"allTweetGraph")
+	nx.write_adjlist(mulTweetGraph,"multimediaTweetGraph")
+
+
